ea0abc8047162416fc782a917a1dce1791933a13
kubra <kubra@DESKTOP-K1F6O3C>
2024-08-19 18:20:43 +0300

    Proje dosyası ekle.

---
+++ list_2_/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Kullanıcıdan 5 kahve ismi alarak bir liste oluştur
+        List<string> kahveListesi = new List<string>();
+
+        Console.WriteLine("Lütfen 5 kahve ismi girin:");
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write($"Kahve {i}: ");
+            string kahve = Console.ReadLine();
+            kahveListesi.Add(kahve);
+        }
+
+        Console.WriteLine("\nGirdiğiniz kahveler:");
+        foreach (string kahve in kahveListesi)
+        {
+            Console.WriteLine("- " + kahve);
+        }
+    }
+}
